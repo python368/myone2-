2e0cfed77347c14911d078f681439e4fdcab5865
John Doe <python1233456789shen@gmail.com>
2025-05-28 14:21:42 +0800

    python

--- 自己写的/koch/koch.py
+++ 自己写的/koch/koch.py
@@ -15,4 +15,4 @@ def drawKochSF(x1,y1,x2,y2, t):
         # 第3个片段
         drawKochSF(p2[0], p2[1], p3[0], p3[1], t)
         # 第4个片段
-        drawKochSF(p3[0], p3[1], x2, y2, t);
+        drawKochSF(p3[0], p3[1], x2, y2, t)
