cfd102d0d68b59c6d30dfa58621af0689b036194
John Doe <python1233456789shen@gmail.com>
2025-05-31 22:11:26 +0800

    妈妈是永生的人

---
+++ fixed_code.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, opt, l, r;
+    cin >> n >> m;
+    cin >> opt >> l >> r;
+    
+    if (opt == 1) {
+        cout << (1 + r) << endl;
+    }
+    
+    return 0;
+}
