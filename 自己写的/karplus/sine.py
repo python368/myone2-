5efe93fe510826e7314c940d769488560fa2f8e8
John Doe <python1233456789shen@gmail.com>
2025-07-23 15:07:41 +0800

    Implement code changes to enhance functionality and improve performance

--- 自己写的/karplus/sine.py
+++ 自己写的/karplus/sine.py
@@ -5,4 +5,8 @@ sRate = 44100
 nSamples = sRate * 5
 x = np.arange(nSamples)/float(sRate)
 vals = np.sin(2.0*math.pi*220.0*x)
-data = np.array(vals*32767, 'int16').tostring()
+data = np.array(vals*32767, 'int16').tobytes()
+file = wave.open('sine220.wav', 'wb')
+file.setparams((1, 2, sRate, nSamples, 'NONE', 'uncompressed'))
+file.writeframes(data)
+file.close()
